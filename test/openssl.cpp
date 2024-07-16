0d1f14215972f6d3411ba6abfb3bdd997c4fc91d
Rich Logan <rich@richlogan.co.uk>
2024-07-16 11:43:49 +0100

    Extensible test suite

---
+++ test/openssl.cpp
@@ -0,0 +1,17 @@
+#include <doctest/doctest.h>
+#include "sframe.h"
+#include "openssl.h"
+#include <openssl/crypto.h>
+
+using namespace sframe::provider::openssl;
+
+TEST_CASE("FIPS")
+{
+  const auto* require = std::getenv("REQUIRE_FIPS");
+  if (require && FIPS_mode() == 0) {
+    REQUIRE(FIPS_mode_set(1) == 1);
+  }
+}
+
+TYPE_TO_STRING_AS("OpenSSL", OpenSSLProvider);
+TEST_CASE_TEMPLATE_INVOKE(test_suite, OpenSSLProvider);
